71465c6844511e90c3516c30ac339b7688c9e6b5
Mehmet Ozgul <91568457+mehmetozguldev@users.noreply.github.com>
2023-12-09 22:36:05 +0300

    Validations added for dataSchema

--- models/model.js
+++ models/model.js
@@ -1,14 +1,20 @@
+"use strict"
+
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
     name: {
         required: true,
         type: String,
+        minlength: 3,
+        maxlength: 50,
     },
     age: {
         required: true,
         type: Number,
-    }
-})
+        min: 18,
+        max: 120,
+    },
+});
 
 module.exports = mongoose.model('Data', dataSchema);
