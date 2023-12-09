8c77fdfd7340f260f2c93a50e8a3cab584cb6c96
Mehmet Ozgul <91568457+mehmetozguldev@users.noreply.github.com>
2023-12-09 22:07:34 +0300

    Update by id method created

--- routes/routes.js
+++ routes/routes.js
@@ -40,8 +40,20 @@ router.get('/getOne/:id', async (req, res) => {
 });
 
 // Update by ID method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API');
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 // Delete by ID method
